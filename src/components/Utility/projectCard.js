6e2f37a6a5585aa0f100c9701aa0c64e1b41bd66
Rohan Prasad <rohanpd.work@gmail.com>
2020-12-03 13:05:58 +0530

    add creative layout

---
+++ src/components/Utility/projectCard.js
@@ -0,0 +1,44 @@
+import React from "react"
+import {
+  FaBlog,
+  FaBlogger,
+  FaDochub,
+  FaExternalLinkAlt,
+  FaGithub,
+  FaLink,
+  FaSitemap,
+  FaUnlink,
+} from "react-icons/fa"
+import styled from "styled-components"
+
+export default function ProjectCard() {
+  const s = { height: 20, margin: "0.5rem 0.5rem", cursor: "pointer" }
+
+  return (
+    <StyledCard>
+      <h3>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </h3>
+      <span>
+        <FaLink style={s} />
+      </span>
+      <span>
+        <FaGithub style={s} />
+      </span>
+      <span>
+        <FaSitemap style={s} />
+      </span>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+    </StyledCard>
+  )
+}
+
+const StyledCard = styled.section`
+  max-width: 50rem;
+`
---
+++ src/pages/creative.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { SEO } from "../components"
+import Container from "../components/container"
+
+import ProjectCard from "../components/Utility/projectCard"
+
+export default function Creative() {
+  return (
+    <Container>
+      <SEO title="Creative" />
+      <h1>// some dev projects</h1>
+      <ProjectCard />
+
+      <h1>// some design projects</h1>
+      <ProjectCard />
+    </Container>
+  )
+}
